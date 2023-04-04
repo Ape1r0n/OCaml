9fc4a2ee3d1dc5c1d9ce5693844851d94cee85c8
Didubeli Soso <48657340+Ape1r0n@users.noreply.github.com>
2023-04-04 17:52:20 +0400

    Updated ListMismash.ml

--- ListMismash.ml
+++ ListMismash.ml
@@ -1,2 +1,4 @@
 let rec interleave2 x y = match x with [] -> y | h::t -> h :: interleave2 t y;;
 let rec interleave3 a b c = match a with [] -> interleave2 b c | h::t -> h :: interleave3 t b c;;
+
+let rec interleave3_oneliner a b c = match a with [] -> (match b with [] -> c | hd::tl -> hd :: interleave3_oneliner [] tl c) | h::t -> h :: interleave3_oneliner t b c;;
