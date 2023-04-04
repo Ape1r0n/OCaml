afa44adb13f052ef74ee2de85cdb7dd266a00ef0
Didubeli Soso <48657340+Ape1r0n@users.noreply.github.com>
2023-04-04 17:59:07 +0400

    Misunderstood exercise, corrected now

--- ListMismash.ml
+++ ListMismash.ml
@@ -1,4 +1,4 @@
-let rec interleave2 x y = match x with [] -> y | h::t -> h :: interleave2 t y;;
-let rec interleave3 a b c = match a with [] -> interleave2 b c | h::t -> h :: interleave3 t b c;;
+let rec interleave2 x y = match x with [] -> y | h::t -> h :: interleave2 y t
+let rec interleave3 a b c = match a with [] -> interleave2 b c | h::t -> h :: interleave3 b c t
 
-let rec interleave3_oneliner a b c = match a with [] -> (match b with [] -> c | hd::tl -> hd :: interleave3_oneliner [] tl c) | h::t -> h :: interleave3_oneliner t b c;;
+let rec interleave3_oneliner a b c = match a with [] -> (match b with [] -> c | hd::tl -> hd :: interleave3_oneliner [] c tl) | h::t -> h :: interleave3_oneliner b c t
